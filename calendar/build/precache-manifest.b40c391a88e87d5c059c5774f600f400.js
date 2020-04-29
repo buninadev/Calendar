4a7af0c023d1a53c2ceaadd600c61f3f049b3342
buninadev <55672459+buninadev@users.noreply.github.com>
2020-04-29 10:27:01 +0200

    New build

---
+++ calendar/build/precache-manifest.b40c391a88e87d5c059c5774f600f400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14deeca9ef2c4f9efcfd43b5a9c46f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2486e9308d93fe79923e",
+    "url": "./static/css/main.d7922e93.chunk.css"
+  },
+  {
+    "revision": "f07adc5857d11faa4de5",
+    "url": "./static/js/2.394be7ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.394be7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2486e9308d93fe79923e",
+    "url": "./static/js/main.eab45e9a.chunk.js"
+  },
+  {
+    "revision": "51d265698dd7debbeded",
+    "url": "./static/js/runtime-main.e85d5ff6.js"
+  }
+]);
